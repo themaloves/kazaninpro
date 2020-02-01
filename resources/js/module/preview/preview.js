75ea4eb89a003e03046e95b1b07967d5c3320b80
developer <dipper.dev.intehart@gmail.com>
2020-02-01 17:30:36 +0300

    add init js

---
+++ resources/js/module/preview/preview.js
@@ -0,0 +1,4 @@
+let $ = require('jquery');
+
+let a = $('.preview');
+console.log(a);
--- resources/views/page/object/show.blade.php
+++ resources/views/page/object/show.blade.php
@@ -6,20 +6,32 @@
     <div class="collection-object__background-wrap"></div>
 
     @foreach($cards as $card)
+
         <a class="collection-object__items" href="{{ route('object.show-card', ['object' => $card->object, 'card' => $card->id]) }}">
+
             <div class="collection-object__background">
                 @if($card->attachment->count() > 0)
                     <img src="{{ $card->attachment->first()->url() }}" alt="">
                 @endif
             </div>
-            <span class="collection-object__title">{{ View::trans($card->title['ru'], $card->title['en']) }}</span>
-            <span class="collection-object__description">
-                {{ View::trans($card->description['ru'], $card->description['en']) }}
-            </span>
-            <span class="collection-object__address">
-                {{ View::trans($card->address['ru'], $card->address['en']) }}
-            </span>
+
+            <div class="collection-object__wrap">
+
+                <span class="collection-object__title">
+                    {{ View::trans($card->title['ru'], $card->title['en']) }}
+                </span>
+
+                <span class="collection-object__description">
+                    {{ View::trans($card->description['ru'], $card->description['en']) }}
+                </span>
+
+                <span class="collection-object__address">
+                    {{ View::trans($card->address['ru'], $card->address['en']) }}
+                </span>
+
+            </div>
         </a>
+
     @endforeach
 
 </div>
