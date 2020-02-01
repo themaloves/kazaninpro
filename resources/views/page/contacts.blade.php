eb7d97447ea03acef6872b25552420e417eb37a7
developer <dipper.dev.intehart@gmail.com>
2020-02-01 21:50:02 +0300

    refactoring contacts page
    structure and fonts, padding

--- resources/views/page/contacts.blade.php
+++ resources/views/page/contacts.blade.php
@@ -3,50 +3,52 @@
 @section('content')
     <section class="contacts">
 
-        <div class="contacts__background"></div>
+        @include('layout.header')
 
-        <div class="contacts__wrap">
+        <div class="contacts__items inner-content">
 
-            <div class="contacts__director">
-                <h1>{{ __('contacts.director.name') }}</h1>
-                <p class="contacts__description">
-                    {{ __('contacts.director.last-name') }}
-                    {{ __('contacts.director.first-name') }}
-                    {{ __('contacts.director.middle-name') }}
-                    <br>
-                    {{ __('contacts.director.tel') }}
-                </p>
+                <div class="contacts__item">
+                    <h1 class="contacts__title">{{ __('contacts.director.name') }}</h1>
+                    <p class="contacts__description">
+                        {{ __('contacts.director.last-name') }}
+                        {{ __('contacts.director.first-name') }}
+                        {{ __('contacts.director.middle-name') }}
+                        <br>
+                        {{ __('contacts.director.tel') }}
+                    </p>
 
-            </div>
+                </div>
 
-            <div class="contacts__engineer">
-                <h1>{{ __('contacts.engineer.name') }}</h1>
-                <p class="contacts__description">
-                    {{ __('contacts.engineer.last-name') }}
-                    {{ __('contacts.engineer.first-name') }}
-                    {{ __('contacts.engineer.middle-name') }}
-                    <br>
-                    {{ __('contacts.engineer.tel') }}
-                </p>
-            </div>
+                <div class="contacts__item">
+                    <h1 class="contacts__title">{{ __('contacts.engineer.name') }}</h1>
+                    <p class="contacts__description">
+                        {{ __('contacts.engineer.last-name') }}
+                        {{ __('contacts.engineer.first-name') }}
+                        {{ __('contacts.engineer.middle-name') }}
+                        <br>
+                        {{ __('contacts.engineer.tel') }}
+                    </p>
+                </div>
 
-            <div class="contacts__address">
-                <h1>{{ __('contacts.address.name') }}</h1>
-                <p class="contacts__description">
-                    {{ __('contacts.address.location') }},
-                    <br>
-                    {{ __('contacts.address.street') }}
-                </p>
-            </div>
+                <div class="contacts__item">
+                    <h1 class="contacts__title">{{ __('contacts.address.name') }}</h1>
+                    <p class="contacts__description">
+                        {{ __('contacts.address.location') }},
+                        <br>
+                        {{ __('contacts.address.street') }}
+                    </p>
+                </div>
 
-            <div class="contacts__email">
-                <h1>{{ __('contacts.mail.name') }}</h1>
-                <p class="contacts__description">
-                    {{ __('contacts.mail.email') }}
-                </p>
-            </div>
+                <div class="contacts__item">
+                    <h1 class="contacts__title">{{ __('contacts.mail.name') }}</h1>
+                    <p class="contacts__description">
+                        {{ __('contacts.mail.email') }}
+                    </p>
+                </div>
 
         </div>
+
+        <div class="spacer"></div>
 
     </section>
 @endsection
