bd8f38f5db5c67c57dafbc393019ab102dc29f13
developer <dipper.dev.intehart@gmail.com>
2020-02-02 21:24:50 +0300

    refactoring section direction activity

--- resources/views/page/about.blade.php
+++ resources/views/page/about.blade.php
@@ -13,69 +13,12 @@
 
         @include('parts.geography-projects.geography-projects')
 
+        @include('parts.direction-activity.direction-activity')
+
 {{--        <span class="about-us__page-decor">О нас</span>--}}
 
 {{--
 
-{{--        <div class="geography-project">--}}
-
-{{--            <div class="geography-project__title">--}}
-{{--                <h1 class="heading--normal-size">География проектов</h1>--}}
-{{--            </div>--}}
-
-{{--            <div class="geography-project__description">--}}
-{{--                <p class="paragraph--normal-size">--}}
-{{--                    За короткую историю, нашими специалистами выполнялись работы в Москве и Московской области,--}}
-{{--                    Нижнем Новгороде, Перми, Иркутске, Самаре, а также почти во всех районах Республики Татарстан.--}}
-{{--                </p>--}}
-{{--            </div>--}}
-
-{{--        </div>--}}
-
-{{--        <div class="activity">--}}
-{{--            <div class="activity__head-title">--}}
-{{--                <h2 class="heading--normal-size">Направление деятельности</h2>--}}
-{{--            </div>--}}
-{{--            <div class="activity__items-wrap">--}}
-{{--                <div class="activity__items">--}}
-{{--                    <div class="activity__icon">--}}
-{{--                        <i class="fas fa-pen"></i>--}}
-{{--                    </div>--}}
-{{--                    <div class="activity__title">Проектирование</div>--}}
-{{--                </div>--}}
-{{--                <div class="activity__items">--}}
-{{--                    <div class="activity__icon">--}}
-{{--                        <i class="fas fa-compress-arrows-alt"></i>--}}
-{{--                    </div>--}}
-{{--                    <div class="activity__title">Обследование</div>--}}
-{{--                </div>--}}
-{{--                <div class="activity__items">--}}
-{{--                    <div class="activity__icon">--}}
-{{--                        <i class="fas fa-eye"></i>--}}
-{{--                    </div>--}}
-{{--                    <div class="activity__title">Мониторинг состояния</div>--}}
-{{--                </div>--}}
-{{--                <div class="activity__items">--}}
-{{--                    <div class="activity__icon">--}}
-{{--                        <i class="fas fa-ruler-combined"></i>--}}
-{{--                    </div>--}}
-{{--                    <div class="activity__title">Реконструкция</div>--}}
-{{--                </div>--}}
-{{--                <div class="activity__items">--}}
-{{--                    <div class="activity__icon">--}}
-{{--                        <i class="fas fa-search"></i>--}}
-{{--                    </div>--}}
-{{--                    <div class="activity__title">Независимая экспертиза</div>--}}
-{{--                </div>--}}
-{{--                <div class="activity__items">--}}
-{{--                    <div class="activity__icon">--}}
-{{--                        <i class="fas fa-file-alt"></i>--}}
-{{--                    </div>--}}
-{{--                    <div class="activity__title">Испытание кострукций</div>--}}
-{{--                </div>--}}
-{{--            </div>--}}
-
-{{--        </div>--}}
 
 {{--        <div class="our-clients">--}}
 {{--            <div class="our-clients__title">--}}
---
+++ resources/views/parts/direction-activity/direction-activity.blade.php
@@ -0,0 +1,32 @@
+<div class="direction-activity inner-content">
+
+    <h2 class="direction-activity__title">Направление деятельности</h2>
+
+    <div class="direction-activity__items">
+        <div class="direction-activity__item">
+            <i class="direction-activity__icon fas fa-pen"></i>
+            <span class="direction-activity__description">Проектирование</span>
+        </div>
+        <div class="direction-activity__item">
+            <i class="direction-activity__icon fas fa-compress-arrows-alt"></i>
+            <span class="direction-activity__description">Обследование</span>
+        </div>
+        <div class="direction-activity__item">
+            <i class="direction-activity__icon fas fa-eye"></i>
+            <span class="direction-activity__description">Мониторинг состояния</span>
+        </div>
+        <div class="direction-activity__item">
+            <i class="direction-activity__icon fas fa-ruler-combined"></i>
+            <span class="direction-activity__description">Реконструкция</span>
+        </div>
+        <div class="direction-activity__item">
+            <i class="direction-activity__icon fas fa-search"></i>
+            <span class="direction-activity__description">Независимая экспертиза</span>
+        </div>
+        <div class="direction-activity__item">
+            <i class="direction-activity__icon fas fa-file-alt"></i>
+            <span class="direction-activity__description">Испытание кострукций</span>
+        </div>
+    </div>
+
+</div>
