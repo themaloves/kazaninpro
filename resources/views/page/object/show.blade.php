b3aa0b8d5384b3b3ca9560807c4dfa415acae2d1
developer <dipper.dev.intehart@gmail.com>
2020-02-02 23:12:21 +0300

    init object

--- resources/views/page/object/show.blade.php
+++ resources/views/page/object/show.blade.php
@@ -1,38 +1,43 @@
-{{--<h1>{{ $title }}</h1>--}}
+@section('title', 'Объекты')
 @extends('layout')
 @section('content')
 <div class="collection-object">
 
-    <div class="collection-object__background-wrap"></div>
+        @include('layout.header')
 
-    @foreach($cards as $card)
+    <div class="inner-content">
 
-        <a class="collection-object__items" href="{{ route('object.show-card', ['object' => $card->object, 'card' => $card->id]) }}">
+        <div class="collection-object__items">
+            @foreach($cards as $card)
 
-            <div class="collection-object__background">
-                @if($card->attachment->count() > 0)
-                    <img src="{{ $card->attachment->first()->url() }}" alt="">
-                @endif
-            </div>
+                <a class="collection-object__item" href="{{ route('object.show-card', ['object' => $card->object, 'card' => $card->id]) }}">
 
-            <div class="collection-object__wrap">
+                    <div class="collection-object__background">
+                        @if($card->attachment->count() > 0)
+                            <img src="{{ $card->attachment->first()->url() }}" alt="">
+                        @endif
+                    </div>
+
+                    <div class="collection-object__wrap">
 
                 <span class="collection-object__title">
                     {{ View::trans($card->title['ru'], $card->title['en']) }}
                 </span>
 
-                <span class="collection-object__description">
+                        <span class="collection-object__description">
                     {{ View::trans($card->description['ru'], $card->description['en']) }}
                 </span>
 
-                <span class="collection-object__address">
+                        <span class="collection-object__address">
                     {{ View::trans($card->address['ru'], $card->address['en']) }}
                 </span>
 
-            </div>
-        </a>
+                    </div>
+                </a>
 
-    @endforeach
+            @endforeach
+        </div>
+    </div>
 
 </div>
 @endsection
