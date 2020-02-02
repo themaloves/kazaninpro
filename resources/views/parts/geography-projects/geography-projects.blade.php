8708bb7ea0fe62ac58eae0e260ad9f15907fa392
developer <dipper.dev.intehart@gmail.com>
2020-02-02 20:30:36 +0300

    refactoring section geography-projects

---
+++ resources/views/parts/geography-projects/geography-projects.blade.php
@@ -0,0 +1,10 @@
+<div class="geography-project inner-content">
+
+    <h2 class="geography-projects__title">География проектов</h2>
+
+    <p class="geography-projects__description">
+        За короткую историю, нашими специалистами выполнялись работы в Москве и Московской области,
+        Нижнем Новгороде, Перми, Иркутске, Самаре, а также почти во всех районах Республики Татарстан.
+    </p>
+
+</div>
