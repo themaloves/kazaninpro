198d2ca44425fb78600a3bbdf801b6aaad1eb9a8
developer <dipper.dev.intehart@gmail.com>
2020-02-07 09:00:17 +0300

    refactoring english version

---
+++ resources/lang/en/about-as.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+    'column-top' => [
+        'title' => 'About us',
+        'text-first' => 'The founders of the company - Roman Trizna and Anton Gizdatullin, who have been working in the field of survey and design of buildings since 2006, merged in 2014 and began working under the common brand name «Kazan Engineering Project». For several years, we were able to convince dozens of customers of our responsibility and competence.',
+        'text-second' => 'Today, the Kazan engineering project is a team of professionals specializing in the examination and independent examination of buildings and structures, as well as on the development of draft decisions on the strengthening and reconstruction of buildings and structures for various purposes.',
+    ],
+    'column-bottom' => [
+        'text-first' => 'The initial and main specialization of our company is a technical survey of buildings, conducted to assess their actual technical condition, the possibility of operation and the need for repair work. Over time, based on the results of our surveys, we began to develop projects for the strengthening and reconstruction of buildings, and subsequently proceeded to the design of new facilities.',
+        'text-second' => 'Employees are actively engaged in scientific research, which allows qualified research related to testing and experimental assessment of the compliance of building structures and materials with design parameters. Our experts conduct testing of reinforcement, reinforcement, reinforcement made of composite glass and basalt-plastic reinforcement and others.',
+        'button-column' => 'Read completely',
+    ],
+    'geography-projects' => [
+        'title' => 'Projects Geography',
+        'description' => 'Our experts work in Moscow and the Moscow region, Nizhny Novgorod, Perm, Irkutsk, Samara, as well as in almost all settlements of the Republic of Tatarstan.',
+        '' => ''
+    ],
+    'direction-activity' => [
+        'title' => 'Area of activity',
+        'description1' => 'Engineering',
+        'description2' => 'Research',
+        'description3' => 'Status Monitoring',
+        'description4' => 'Reconstruction',
+        'description5' => 'Independent examination',
+        'description6' => 'Testing of the structures',
+    ]
+];
